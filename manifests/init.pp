dba354c14a9a3dfea59d7a46ceb15b835ffae78f
ryan00 <ryan@cylentsystems.com>
2015-01-20 10:26:55 -0600

    added version tag

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 # Install GPGTools.
-class gpgtools {
+class gpgtools ($version = '2014.12-b4'){
   package { 'GPGTools':
     name     => 'GPGTools.pkg',
     provider => 'pkgdmg',
-    source   => 'https://releases.gpgtools.org/GPG%20Suite%20-%202013.10.22.dmg',
+    source   => "https://releases.gpgtools.org/GPG%20Suite%20-%20${version}.dmg",
   }
 }
